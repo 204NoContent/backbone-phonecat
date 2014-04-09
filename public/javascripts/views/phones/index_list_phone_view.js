380c185a8e297c36f00646c1de08a714960bcb83
Aaron <aaron.oconnell@gmail.com>
2014-04-09 15:01:51 -0700

    step-6 phone images and links

--- public/javascripts/views/phones/index_list_phone_view.js
+++ public/javascripts/views/phones/index_list_phone_view.js
@@ -1,5 +1,9 @@
 PhonesIndexListPhoneView = Backbone.View.extend({
 
+    events: {
+        'click a': 'navigate'
+    },
+
     initialize: function () {
         this.listenTo(this.model, 'remove', this.remove, this);
         this.render();
@@ -7,5 +11,10 @@ PhonesIndexListPhoneView = Backbone.View.extend({
 
     render: function () {
         this.$el.html(JST['phones/index_list_phone'](this.model));
+    },
+
+    navigate: function (event) {
+        event.preventDefault();
+        App.navigate(event.currentTarget.pathname, { trigger: true });
     }
 });
